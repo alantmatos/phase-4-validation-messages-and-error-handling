21307a3d5d7e46a64ca94aeda64a7610bea47b74
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-07-20 15:52:16 -0400

    commit tree 173f58e121cec974d461fefb8cd94d2bc7591c4f

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   wrap_parameters false
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_baddata_response
   
   def index
     movies = Movie.all
@@ -7,11 +8,15 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
   end
 
   private
+
+  def handle_baddata_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
